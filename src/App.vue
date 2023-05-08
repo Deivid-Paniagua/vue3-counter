3d062c950caf99f45b81ef665f3a787e0961cbb6
Deivid Paniagua Porras <71730434+Deivid16@users.noreply.github.com>
2023-05-07 19:43:31 -0600

    Vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data: () => ({ 
+    contador: 10, }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    disminuir() {
+      this.contador--;
+    },
+    resetear() {
+      this.contador=10;
+    },
+  },
+};
+</script>
+
+
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="LOGO VUE" width="125" height="125" />
+    <small><h1>Hola Soy Paniagua y este es mi primer contador</h1> </small>
+    <hr />
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="disminuir()">-1</button>
+    <button @click="resetear()">Reset</button>
+  </div>
+</template>
